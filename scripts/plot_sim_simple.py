c0d12095e58f3f763dcba51f668fb5c3af8b7534
Eric Pooser <pooser@jlab.org>
2017-08-14 12:00:14 -0400

    Finish plot for simplified simulation

---
+++ scripts/plot_sim_simple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+sdis, shit = np.loadtxt("../data/sim_simple_straight.dat", skiprows=1, unpack=True)
+ndis, nhit = np.loadtxt("../data/sim_simple_nose.dat", skiprows=1, unpack=True)
+
+shit_err = np.sqrt(shit)
+nhit_err = np.sqrt(nhit)
+
+afs = 20
+
+plt.figure()
+
+plt.errorbar(ndis, nhit, yerr=nhit_err, fmt='o', label='Nose Section', color='g')
+plt.errorbar(sdis, shit, yerr=shit_err, fmt='o', label='Straight Section', color='b')
+
+plt.title('Simplified Model', size=afs)
+plt.xlim(0, 300)
+plt.xlabel('Distance of Source From SiPM (cm)', size=afs)
+plt.ylim(0, 1600)
+plt.ylabel('Number of Hits in SiPM', size=afs)
+
+plt.legend(loc=1, numpoints=1)
+
+plt.show()
