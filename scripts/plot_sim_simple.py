b61071f885e73e2fe9ed2f59287a753306645e13
Eric Pooser <pooser@jlab.org>
2017-08-15 13:35:56 -0400

    Add new figures and tweaks to data files and script

--- scripts/plot_sim_simple.py
+++ scripts/plot_sim_simple.py
@@ -3,11 +3,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sdis, shit = np.loadtxt("../data/sim_simple_straight.dat", skiprows=1, unpack=True)
-ndis, nhit = np.loadtxt("../data/sim_simple_nose.dat", skiprows=1, unpack=True)
-
-shit_err = np.sqrt(shit)
-nhit_err = np.sqrt(nhit)
+sdis, shit, shit_err = np.loadtxt("../data/sim_simple_straight.dat", skiprows=1, unpack=True)
+ndis, nhit, nhit_err = np.loadtxt("../data/sim_simple_nose.dat", skiprows=1, unpack=True)
 
 afs = 20
 
@@ -17,11 +14,11 @@ plt.errorbar(ndis, nhit, yerr=nhit_err, fmt='o', label='Nose Section', color='g'
 plt.errorbar(sdis, shit, yerr=shit_err, fmt='o', label='Straight Section', color='b')
 
 plt.title('Simplified Model', size=afs)
-plt.xlim(0, 275)
-plt.xlabel('Distance of Source From SiPM (cm)', size=afs)
+plt.xlim(0, 400)
+plt.xlabel('Distance of Source From SiPM (mm)', size=afs)
 plt.ylim(0, 1600)
 plt.ylabel('Number of Hits in SiPM', size=afs)
 
-plt.legend(loc=2, numpoints=1)
+plt.legend(loc=1, numpoints=1)
 
 plt.show()
